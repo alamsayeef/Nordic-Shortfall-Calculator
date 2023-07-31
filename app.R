6c1d6f47700babb0bfd59aab1de6f48fd2f31de5
mohasal12 <65837783+alamsayeef@users.noreply.github.com>
2023-07-31 12:35:33 +0200

    beta_v2.1
    added comQale function and few headings

--- app.R
+++ app.R
@@ -1,6 +1,104 @@
+# libraries ----
 library(shiny)
 library(shinydashboard)
 
+# input data ----
+ref_df = read.csv("https://github.com/bitowaqr/shortfall/raw/main/app/data/ref_df_appended.csv")
+mvh_df = read.csv("https://github.com/bitowaqr/shortfall/raw/main/app/data/mvh_df.csv")
+
+# compute QALE ----
+compQale = function(ons_df,
+                    prop_female = 0.5,
+                    start_age = 50,
+                    disc_rate = 0.035,
+                    utils = "cw") {
+  compQaleInternal = function(ons_df,
+                              prop_female = 0.5,
+                              start_age = 50,
+                              disc_rate = 0.035,
+                              utils = "cw") {
+    ons_df = ons_df[ons_df$age >= start_age, ]
+    ons_df = ons_df[order(ons_df$age), ]
+    df_female = ons_df[ons_df$sex == "female", c("age", utils, "lx", "dx", "mx", "ex")]
+    df_male = ons_df[ons_df$sex == "male", c("age", utils, "lx", "dx", "mx", "ex")]
+    
+    df_comp = data.frame(
+      age = df_female$age,
+      utils = (1 - prop_female) * df_male[, utils]  + prop_female * df_female[, utils],
+      lx = (1 - prop_female) * df_male$lx  + prop_female * df_female$lx,
+      dx = (1 - prop_female) * df_male$dx  + prop_female * df_female$dx,
+      mx = (1 - prop_female) * df_male$mx  + prop_female * df_female$mx,
+      ex = (1 - prop_female) * df_male$ex  + prop_female * df_female$ex
+    )
+    
+    # person years in year i
+    df_comp$Lx = NA
+    for (i in 2:nrow(df_comp)) {
+      df_comp$Lx[i - 1] = df_comp$lx[i] + (0.5 * df_comp$dx[i - 1])
+    }
+    df_comp$Lx[nrow(df_comp)] = (df_comp$lx[nrow(df_comp)] - df_comp$dx[nrow(df_comp)]) + (0.5 * df_comp$dx[nrow(df_comp)])
+    
+    # person QALYs in year i
+    df_comp$Yx = df_comp$utils * df_comp$Lx
+    
+    # apply discounting
+    v_disc <- 1 / (1 + disc_rate) ^ (0:(length(df_comp$Yx) - 1))
+    df_comp$Yx = df_comp$Yx * v_disc
+    
+    # remaining person QALYs?
+    df_comp$Nx = NA
+    df_comp$Nx[nrow(df_comp)] = df_comp$Yx[nrow(df_comp)]
+    for (i in nrow(df_comp):2) {
+      df_comp$Nx[i - 1] = df_comp$Yx[i - 1] + df_comp$Nx[i]
+    }
+    
+    # Quality adjusted life expectancy
+    df_comp$Qx = df_comp$Nx / df_comp$lx
+    
+    
+    q_factor = sum(df_comp$Yx) / df_comp$Qx[1]
+    
+    df_comp$qalys_by_year = df_comp$Yx / q_factor
+    df_comp$cumulative_qalys = cumsum(df_comp$qalys_by_year)
+    
+    # cumulative survival function
+    df_comp$S = 1 - df_comp$mx
+    df_comp$S_cumulativ =  cumprod(df_comp$S)
+    df_comp$hrqol = df_comp$utils
+    
+    df_comp = df_comp[, c("age",
+                          "hrqol",
+                          "ex",
+                          "Qx",
+                          "S_cumulativ",
+                          "cumulative_qalys")]
+    
+    return(df_comp)
+    
+  }
+  
+  
+  qale_male = compQaleInternal(
+    ons_df = ons_df,
+    prop_female = 0,
+    start_age = start_age,
+    disc_rate = disc_rate,
+    utils = utils
+  )
+  qale_female = compQaleInternal(
+    ons_df = ons_df,
+    prop_female = 1,
+    start_age = start_age,
+    disc_rate = disc_rate,
+    utils = utils
+  )
+  qale_mix = qale_male * (1 - prop_female) + qale_female * prop_female
+  
+  return(qale_mix)
+  
+}
+
+# ui ----
 ui = dashboardPage(
   dashboardHeader(title = "Scandinavian Shortfall Calculator",
                   titleWidth = 350),
@@ -44,16 +142,16 @@ ui = dashboardPage(
                       ),
                       box(
                         width = 2,
-                        selectInput(
-                          "den_dropdown",
+                        selectizeInput(
+                          "den_utils",
                           "Select scenario",
-                          selected = "ref",
+                          selected = "dsu_2014",
                           choices = c(
-                            "Reference Case" = "ref",
-                            "Alternative A" = "a",
-                            "Alternative B" = "b",
-                            "Alternative C" = "c",
-                            "Alternative D" = "d"
+                            "Reference case: MVH value set + HSE 2014 ALDVMM model (Hernandez Alava et al)" = "dsu_2014",
+                            "Alternative A: 5L to 3L mapping (Hernandez Alava et al) + HSE 2017-2018" = "dsu",
+                            "Alternative B: 5L to 3L mapping (van Hout et al) + HSE 2017-2018" = "vanHout",
+                            "Alternative C: MVH value set + health state profiles" = "mvh",
+                            "Alternative D: MVH value set + HSE 2012+14" = "tto"
                           )
                         )
                       ),
@@ -498,8 +596,10 @@ ui = dashboardPage(
                 ))
 )
 
+# server ----
 server = function(input, output, session) {
   
 }
 
+# app compiler ----
 shinyApp(ui, server)
